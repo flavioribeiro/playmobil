245d3b4cb24f94fbe2c0216f7f55b9d3c0fdad0f
Marcelo Pereira <marcelopereira@corp.globo.com>
2014-12-02 10:48:53 -0200

    adding project rails with mongoid, railsadmin and resque

--- Gemfile
+++ Gemfile
@@ -2,3 +2,32 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in playmobil.gemspec
 gemspec
+
+gem 'rails', '4.0.4'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+gem 'rails_admin'
+
+# MongoDB
+gem 'mongoid', '>= 4.0.0'
+gem 'bson_ext'
+
+# Resque
+gem 'resque', require: 'resque/server'
+gem 'resque-scheduler'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+
+  # Use debugger
+  gem 'byebug'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Playmobil::Application.config.secret_key_base = '63bdabb3edca69c5aa8b4a942c5440a3ebf714b9ae1020a0bfa80646b698906e6e999b69603a4b2e07280aa6153d252d24019fbb48c6154dc8dd3b2716510a36'
